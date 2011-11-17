233120cc8e7952667496b7656fa4cff7c4be2ca3
darthmolly <mpc3c@virginia.edu>
2011-11-17 17:37:07 -0500

    adding bands migration; removing backup files accidentally committed

--- db/migrate/20111117213012_create_band.rb
+++ db/migrate/20111117213012_create_band.rb
@@ -1,7 +1,15 @@
 class CreateBand < ActiveRecord::Migration
   def self.up
+    create_table :bands do |t|
+      t.string :name
+      t.string :link
+      t.boolean :checkable, :default => false
+      t.boolean :checked_by_default, :default => false
+      t.text :comments
+    end
   end
 
   def self.down
+    drop_table :bands
   end
 end
